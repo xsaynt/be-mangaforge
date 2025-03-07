e635e6faece2230d763dec8cd6012eb325dc7daf
xsaynt <alex-price3@hotmail.co.uk>
2025-03-07 15:59:28 +0000

    multiple files created ready for population

---
+++ db/connection.js
@@ -0,0 +1,22 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+const ENV = process.env.NODE_ENV || 'development';
+
+dotenv.config({
+	path: `${__dirname}/../../.env.${ENV}`,
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+	throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
+const config = {};
+
+if (ENV === 'production') {
+	config.connectionString = process.env.DATABASE_URL;
+	config.max = 2;
+}
+
+const db = new Pool(config);
+
+export default db;
