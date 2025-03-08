8fe70f3f4cedcecda5eabdd734f60c14363d2bbd
xsaynt <alex-price3@hotmail.co.uk>
2025-03-08 14:50:02 +0000

    creating test and dev data for seeding

---
+++ db/data/test-data/users.js
@@ -0,0 +1,22 @@
+export const usersData = [
+	{
+		user_id: 1,
+		username: 'Manga_Lover123',
+		user_password: 'Manga4Life',
+		user_picture:
+			'https://cdn-images.dzcdn.net/images/cover/f3b680b4e27d40a08728d45d95500e9e/500x500-000000-80-0-0.jpg',
+	},
+	{
+		user_id: 2,
+		username: 'Bleach_is_best',
+		user_password: 'password123',
+		user_picture: 'https://images2.alphacoders.com/665/thumb-1920-665898.jpg',
+	},
+	{
+		user_id: 3,
+		username: 'Gojo Lover',
+		user_password: 'minimince',
+		user_picture:
+			'https://i1.sndcdn.com/artworks-vzvYkPmzB5nuo1oT-yg9zfw-t1080x1080.jpg',
+	},
+];
