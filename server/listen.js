d3298a0cb539d5fa28d9cf8d26a4cb61f4d7cbe2
xsaynt <alex-price3@hotmail.co.uk>
2025-03-12 15:37:31 +0000

    removed changes to server file

--- server/listen.js
+++ server/listen.js
@@ -1,14 +1,4 @@
-import app from '../__app__/app.js';
-const express = require('express');
-const dotenv = require('dotenv');
-const authRouter = require('../routers/auth-router.js');
-
-dotenv.config();
-
-app.use(express.json());
-
-app.use('/.', authRouter);
-
+import app from './__app__/app';
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
